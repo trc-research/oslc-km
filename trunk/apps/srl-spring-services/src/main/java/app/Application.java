829357e0c8d29e7499f9b3bca44b77b27736e824
chema <chema.ar@gmail.com>
2019-06-03 19:14:51 +0200

    Updating names and adding swagger support

--- trunk/apps/srl-spring-services/src/main/java/app/Application.java
+++ trunk/apps/srl-spring-services/src/main/java/app/Application.java
@@ -3,6 +3,7 @@ package app;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -14,6 +15,12 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import com.mongodb.MongoClient;
 import com.mongodb.WriteConcern;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -24,12 +31,25 @@ import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @ConfigurationProperties
-@EnableMongoRepositories
 @ComponentScan(basePackages={"com.reusecompany"})
+@EnableSwagger2
 class Application {
 
     public static void main(String[] args) {
+    	//FIXME: hardcoded
+    	//See more: https://www.baeldung.com/spring-boot-context-path
+    	System.setProperty("server.servlet.context-path", "/srl-web");
         SpringApplication.run(Application.class, args);
+    }
+    
+    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
     }
 
 }
